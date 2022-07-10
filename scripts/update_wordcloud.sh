43fff4e470f424d19f8f8275791e8df0a6575d47
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-07-10 17:34:01 -0400

    add wordcloud!

---
+++ scripts/update_wordcloud.sh
@@ -0,0 +1,9 @@
+: '
+Script to update the wordcloud image.
+Requires the wordcloud_cli python tool:
+    pip install wordcloud
+    https://github.com/amueller/word_cloud
+'
+
+cat src/[0-9]-*.tex src/ack.tex src/appendix.tex > src/ALL.tex.temp
+wordcloud_cli --text src/ALL.tex.temp --width 2000 --height 1000 --imagefile src/img/wordcloud.png
