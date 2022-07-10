e7274c01a2d6c4c42bfa4fb426db778bb6d3447e
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-07-10 17:37:13 -0400

    make wordcloud deterministic

--- scripts/update_wordcloud.sh
+++ scripts/update_wordcloud.sh
@@ -6,4 +6,4 @@ Requires the wordcloud_cli python tool:
 '
 
 cat src/[0-9]-*.tex src/ack.tex src/appendix.tex > src/ALL.tex.temp
-wordcloud_cli --text src/ALL.tex.temp --width 2000 --height 1000 --imagefile src/img/wordcloud.png
+wordcloud_cli --text src/ALL.tex.temp --width 2000 --height 1000 --imagefile src/img/wordcloud.png --random_state 1
