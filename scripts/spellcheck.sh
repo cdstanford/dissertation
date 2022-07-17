500472762d61078f2af555635ef96a0b0399a25b
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-07-17 19:37:34 -0400

    var args in spellcheck script

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 : '
 Script to run spellcheck on all .tex source files.
 Requires aspell.
 '
 
-aspell -c -t --home-dir=scripts src/0-abstract.tex
-aspell -c -t --home-dir=scripts src/1-intro.tex
+ASPELL_ARGS="-c -t --dont-backup --home-dir=scripts"
+
+aspell $ASPELL_ARGS src/0-abstract.tex
+aspell $ASPELL_ARGS src/1-intro.tex
