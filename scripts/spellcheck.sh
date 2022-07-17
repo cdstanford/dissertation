153c9fdf2c3d0dece36015a058edbae4a178c127
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-07-17 19:51:36 -0400

    spellcheck section 3

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -9,3 +9,4 @@ ASPELL_ARGS="-c -t --dont-backup --home-dir=scripts"
 aspell $ASPELL_ARGS src/0-abstract.tex
 aspell $ASPELL_ARGS src/1-intro.tex
 aspell $ASPELL_ARGS src/2-background.tex
+aspell $ASPELL_ARGS src/3-rw.tex
