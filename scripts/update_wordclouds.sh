3fd0e01017467a599b5c206dbd0cdef368592725
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-07-19 08:39:36 -0400

    more wordcloud improvements

--- scripts/update_wordclouds.sh
+++ scripts/update_wordclouds.sh
@@ -13,11 +13,14 @@ cat src/ALL.tex.temp \
   | sed -E 's/^%.*//' \
   | sed -E 's/\\label{.*}/ /g' \
   | sed -E 's/\\Cref{.*}/ /g' \
+  | sed -E 's/\\ref{.*}/ /g' \
+  | sed -E 's/\\eqref{.*}/ /g' \
   | sed -E 's/\\cite(Main)?{.*}/ /g' \
   | sed -E 's/\\begin{.*}/ /g' \
   | sed -E 's/\\end{.*}/ /g' \
   | sed -E 's/\\[a-zA-Z]*//g' \
   | sed -E 's/_/ /g' \
+  | sed -E 's/'\''/ /g' \
   > src/ALL.txt.temp
 
 echo "    updating src/BIB.txt.temp"
@@ -27,7 +30,17 @@ cat src/ref.bib \
   > src/BIB.txt.temp
 
 echo "    updating src/img/wordcloud.png"
-wordcloud_cli --text src/ALL.txt.temp --width 2000 --height 1000 --imagefile src/img/wordcloud.png --random_state 1 --stopwords scripts/wordcloud_omit.txt
+wordcloud_cli --text src/ALL.txt.temp \
+  --width 2000 --height 1000 \
+  --imagefile src/img/wordcloud.png \
+  --random_state 1 \
+  --stopwords scripts/wordcloud_omit.txt \
+  --min_word_length 2
 
 echo "    updating src/img/wordcloud_refs.png"
-wordcloud_cli --text src/BIB.txt.temp --width 2000 --height 1000 --imagefile src/img/wordcloud_refs.png --random_state 1 --stopwords scripts/wordcloud_omit.txt
+wordcloud_cli --text src/BIB.txt.temp \
+  --width 2000 --height 1000 \
+  --imagefile src/img/wordcloud_refs.png \
+  --random_state 1 \
+  --stopwords scripts/wordcloud_omit.txt \
+  --min_word_length 2
