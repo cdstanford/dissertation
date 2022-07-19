d97e40bb89b1334cb6a9249b489c33e2927c728a
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-07-18 21:58:37 -0400

    reduce customization in wordcloud (omitted words -> regex replace tex commands)

--- scripts/update_wordclouds.sh
+++ scripts/update_wordclouds.sh
@@ -7,7 +7,19 @@ Requires the wordcloud_cli python tool:
 
 echo "    updating src/ALL.tex.temp"
 cat src/[0-9]-*.tex src/ack.tex src/appendix.tex > src/ALL.tex.temp
+
+echo "    updating src/ALL.txt.temp"
+cat src/ALL.tex.temp \
+  | sed -E 's/\\label{.*}/ /g' \
+  | sed -E 's/\\Cref{.*}/ /g' \
+  | sed -E 's/\\cite(Main)?{.*}/ /g' \
+  | sed -E 's/\\begin{.*}/ /g' \
+  | sed -E 's/\\end{.*}/ /g' \
+  | sed -E 's/\\[a-zA-Z]*//g' \
+  > src/ALL.txt.temp
+
 echo "    updating src/img/wordcloud.png"
-wordcloud_cli --text src/ALL.tex.temp --width 2000 --height 1000 --imagefile src/img/wordcloud.png --random_state 1 --stopwords scripts/wordcloud_omit.txt
+wordcloud_cli --text src/ALL.txt.temp --width 2000 --height 1000 --imagefile src/img/wordcloud.png --random_state 1 --stopwords scripts/wordcloud_omit.txt
+
 echo "    updating src/img/wordcloud_refs.png"
 wordcloud_cli --text src/ref.bib --width 2000 --height 1000 --imagefile src/img/wordcloud_refs.png --random_state 1 --stopwords scripts/wordcloud_omit.txt
