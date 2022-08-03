7c3460e1a8eddf1ba5aea53861c742e31592a570
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-08-03 13:01:20 -0700

    fix bug in update_progress

--- scripts/update_progress.sh
+++ scripts/update_progress.sh
@@ -5,4 +5,4 @@ Script to update progress.txt with total size of all input files
 echo "    updating data/progress.txt"
 echo "===== Writing progress =====" > data/progress.txt
 echo "   words   chars file" >> data/progress.txt
-find . -name '*.tex' | xargs wc -w -c | sort >> data/progress.txt
+find src -name '*.tex' | xargs wc -w -c | sort >> data/progress.txt
